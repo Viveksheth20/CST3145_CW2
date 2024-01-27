da8b7b20c1ab2813f2e587a0b1a6e55b3bce9315
Vivek Sheth <vs586@live.mdx.ac.uk>
2024-01-27 16:59:09 +0000

    Server setup and AWS

--- server.js
+++ server.js
@@ -1,61 +1,55 @@
 const express = require("express");
 const app = express();
-const { MongoClient} = require("mongodb");
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-const dbpass = "Viveksheth@007";
-const prefex ="mongodb+srv://";
-const username = "Viveksheth4u";
-const dburl = "@cluster.msljc14.mongodb.net/";
-const sufix = "?retryWrites=true&w=majority";
-const uri = prefex+username+":"+dbpass+dburl+sufix;
+const dbpass = "Computer-4";
+const uri = `mongodb+srv://studymodule:${dbpass}@cluster0.brnmkmi.mongodb.net/?retryWrites=true&w=majority`;
 const dbName = "Study_Modules";
-const client = new MongoClient(uri);
 
-async function connectToMongoDB() {
-  
-    try {
-      await client.connect();
-      console.log("Connected to the database");
-  
-      return client.db(dbName);
-    } catch (error) {
-      console.error("Error connecting to the database:", error);
-      throw error;
-    }
-  }
-  
-  // Use async function to connect to MongoDB before starting the server
-  async function startServer() {
-    try {
-      const db = await connectToMongoDB();
-  
-      app.param('collectionName', function (req, res, next, collectionName) {
-        req.collection = db.collection(collectionName);
-        return next();
-      });
-  
-      app.get('/collections/:collectionName', function (req, res, next) {
-        req.collection.find({}).toArray(function (err, results) {
-          if (err) {
-            return next(err);
-          }
-          res.send(results);
-        });
-      });
-  
-      app.listen(3000, () => {
-        console.log("Server is listening on http://localhost:3000/collections/collectionName");
-      });
-    } catch (error) {
-      console.error("Failed to start the server:", error);
-    }
-  }
-  
-  // Start the server
-  startServer();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const client = new MongoClient(uri,{serverApi:ServerApiVersion.v1});
+let db = client.db(dbName);
+const prd_collection = db.collection("products");
+const ord_collection = db.collection("orders");
 
-// app.listen(3000,()=>{
-//     console.log("server is listening on 3000 and wed http://localhost:3000/collections/collectionName");
-// });
+app.get('/', function (req, res, next) {
+  res.send('select a collection, e.g., /collection/products')
+});
+
+app.get('/products', async (req, res, next)=> {
+  await client.connect();
+  const connect = await prd_collection.find({}).toArray(function (err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.send(results);
+  });
+  res.json(connect);
+});
+app.get('/orders', async (req, res, next)=> {
+  await client.connect();
+  const connect = await ord_collection.find({}).toArray(function (err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.send(results);
+  });
+  res.json(connect);
+});
+app.get('/orders', async (req, res, next)=> {
+  await client.connect();
+  const OrdersToInsert = req.body;
+  const connect = await ord_collection.insertOne(OrdersToInsert,function (err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.send(results);
+  });
+  res.json(connect);
+});
+
+const post = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server is listening on 3000");
+});
